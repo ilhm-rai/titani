95da74a8c159660c595c5344e7cfbedcd45c14d1
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2020-08-16 22:28:21 +0700

    Update topbar

--- app/Views/templates/topbar.php
+++ app/Views/templates/topbar.php
@@ -1,10 +1,11 @@
 <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white mb-5">
     <div class="container">
-        <a class="navbar-brand" href="<?= base_url('/'); ?>">
+        <a class="navbar-brand" href="<?= base_url('/'); ?>" title="Home">
             <img src="<?= base_url('assets/img/logo_header.png'); ?>" alt="" loading="lazy">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+
+        <button class="btn btn-light d-md-none rounded-circle mr-3" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa fa-bars"></i>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -13,14 +14,14 @@
                     <a class="nav-link nav-hover" href="<?= base_url('about'); ?>">Tentang Titani</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link nav-hover" href="#" role="button">
+                    <a class="nav-link nav-hover" href="<?= base_url('/'); ?>#service" role="button">
                         Layanan
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link nav-hover" href="<?= base_url('blog'); ?>">Blog Tani</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item mr-0">
                     <a class="btn btn-outline-pastel-green rounded-pill mr-2" href="#">Masuk</a>
                     <a class="btn btn-pastel-green rounded-pill" href="#">Daftar</a>
                 </li>
