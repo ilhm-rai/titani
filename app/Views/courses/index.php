fe82229357e72a7144a1a6e8ea4de1d69dfdeb95
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2020-08-19 01:07:17 +0700

    Menambahkan asset course dan melakukan update

--- app/Views/courses/index.php
+++ app/Views/courses/index.php
@@ -3,4 +3,84 @@
 <a href="#" class="btn btn-filter active" role="button">Semua</a>
 <a href="#" class="btn btn-filter" role="button">Gratis</a>
 <a href="#" class="btn btn-filter" role="button">Premium</a>
+<div class="row mt-4">
+    <div class="col-12 col-sm-8 col-md-6 col-lg-4 mb-4">
+        <div class="card card-course">
+            <img src="<?= base_url('assets/img/course1.png'); ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Hidroponik Dasar</h6>
+                <p class="badge badge-course badge-green-pastel">Gratis</p>
+                <div class="card-text">Hidroponik adalah metode penanaman tanaman tanpa menggunakan media tumbuh dari tanah. </div>
+            </div>
+            <div class="card-footer text-center">
+                <a href="#" class="btn btn-custom btn-pastel-green rounded-pill">Mulai Belajar</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-sm-8 col-md-6 col-lg-4 mb-4">
+        <div class="card card-course">
+            <img src="<?= base_url('assets/img/course1.png'); ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Hidroponik Dasar</h6>
+                <p class="badge badge-course badge-green-pastel">Gratis</p>
+                <div class="card-text">Hidroponik adalah metode penanaman tanaman tanpa menggunakan media tumbuh dari tanah. </div>
+            </div>
+            <div class="card-footer text-center">
+                <a href="#" class="btn btn-custom btn-pastel-green rounded-pill">Mulai Belajar</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-sm-8 col-md-6 col-lg-4 mb-4">
+        <div class="card card-course">
+            <img src="<?= base_url('assets/img/course1.png'); ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Hidroponik Lanjutan</h6>
+                <p class="badge badge-course badge-green-pastel">Premium</p>
+                <div class="card-text">Hidroponik adalah metode penanaman tanaman tanpa menggunakan media tumbuh dari tanah. </div>
+            </div>
+            <div class="card-footer text-center">
+                <a href="#" class="btn btn-custom btn-pastel-green rounded-pill">Berlangganan</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-sm-8 col-md-6 col-lg-4 mb-4">
+        <div class="card card-course">
+            <img src="<?= base_url('assets/img/course1.png'); ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Hidroponik Dasar</h6>
+                <p class="badge badge-course badge-green-pastel">Gratis</p>
+                <div class="card-text">Hidroponik adalah metode penanaman tanaman tanpa menggunakan media tumbuh dari tanah. </div>
+            </div>
+            <div class="card-footer text-center">
+                <a href="#" class="btn btn-custom btn-pastel-green rounded-pill">Mulai Belajar</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-sm-8 col-md-6 col-lg-4 mb-4">
+        <div class="card card-course">
+            <img src="<?= base_url('assets/img/course1.png'); ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Hidroponik Dasar</h6>
+                <p class="badge badge-course badge-green-pastel">Gratis</p>
+                <div class="card-text">Hidroponik adalah metode penanaman tanaman tanpa menggunakan media tumbuh dari tanah. </div>
+            </div>
+            <div class="card-footer text-center">
+                <a href="#" class="btn btn-custom btn-pastel-green rounded-pill">Mulai Belajar</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-sm-8 col-md-6 col-lg-4 mb-4">
+        <div class="card card-course">
+            <img src="<?= base_url('assets/img/course1.png'); ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Hidroponik Lanjutan</h6>
+                <p class="badge badge-course badge-green-pastel">Premium</p>
+                <div class="card-text">Hidroponik adalah metode penanaman tanaman tanpa menggunakan media tumbuh dari tanah. </div>
+            </div>
+            <div class="card-footer text-center">
+                <a href="#" class="btn btn-custom btn-pastel-green rounded-pill">Berlangganan</a>
+            </div>
+        </div>
+    </div>
+</div>
 <?= $this->endSection(); ?>
